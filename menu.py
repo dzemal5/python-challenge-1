5cbc2dbfdd042d7b05dabafa4830a6292315c483
dzemal5 <your email>
2024-03-28 23:21:56 -0400

    03_28 Challange 2 initial commit_work in progress

--- menu.py
+++ menu.py
@@ -1,4 +1,5 @@
-[
+""" Create empty list """
+order_list = [
   {
     "Item name": "string",
     "Price": float,
@@ -10,3 +11,22 @@
     "Quantity": int
   },
 ]
+
+for item in order_list:
+    print(f"Here is the menu option {item}")
+
+keep_ordering = True
+while True:
+        keep_ordering = input("Would you like to keep ordering? (Y)es or (N)o ")
+        match keep_ordering.lower():
+            case 'y':
+                place_order = True
+                break
+            case 'n':
+                place_order = False
+                break
+        
+            case _:
+                print("Please retry selecting only Y or N")
+      
+
